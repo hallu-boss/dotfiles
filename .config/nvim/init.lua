ad2e2283cd0d6e1d48654df3fe1fcc048c124a60
ph <pawelhase@gmail.com>
2026-04-21 00:03:48 +0200

    nvim tmux integration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -30,10 +30,17 @@ require("oil").setup({
 })
 
 vim.g.mapleader = ' '
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
+
+
 vim.keymap.set('n', '<leader>e',   ':Oil .<CR>')
 vim.keymap.set('n', '<leader>E',   ':Oil<CR>')
 vim.keymap.set('n', '<leader>f',   ':Telescope find_files<CR>')
 vim.keymap.set('n', '<leader>b',   ':Telescope buffers<CR>')
 vim.keymap.set('n', '<leader>/',   ':Telescope live_grep<CR>')
 vim.keymap.set('n', '<leader>g',   ':Telescope git_branches<CR>')
+vim.keymap.set('n', '<leader>?',   ':Telescope keymaps<CR>')
 vim.keymap.set('n', '<leader>\'',  ':Telescope resume<CR>')
